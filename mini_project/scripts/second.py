7bc8911724cf59aba08315aecd71aa6c55118121
Kevin <saltytrain2@gmail.com>
2020-09-26 16:50:56 -0500

    new stuff

--- mini_project/scripts/second.py
+++ mini_project/scripts/second.py
@@ -4,15 +4,17 @@ import rospy
 from sensor_msgs.msg import Joy
 
 def callback(data):
-    pub.publish(100 * data.axes[1])
-    pub.publish(100 * data.axes[4])
+    100 * data.axes[1]
+    100 * data.axes[4]
 
 def start():
     rospy.init_node('display', anonymous = True)
     rospy.Subscriber("joy", Joy, callback)
-    global pub
-    pub = rospy.Publisher('output', Joy, queue_size = 10)
     rospy.spin()
+    rospy.Publisher('output', Joy, queue_size = 10)
+    while not rospy.is_shutdown():
+        pub.publish(100 * data.axes[1])
+        pub.publish(100 * data.axes[4])
 
 if __name__ == '__main__':
     start()
